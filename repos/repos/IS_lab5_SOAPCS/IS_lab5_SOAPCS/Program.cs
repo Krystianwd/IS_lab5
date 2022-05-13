7c61b840168dd2e4f7e1ffe6f691d0b23aca8ff2
Krystianwd <krystian.debiec999@gmail.com>
2022-05-13 12:22:43 +0200

    Initial commit

---
+++ repos/repos/IS_lab5_SOAPCS/IS_lab5_SOAPCS/Program.cs
@@ -0,0 +1,22 @@
+﻿using ServiceReference1;
+using System;
+using System.Threading.Tasks;
+
+namespace IS_lab5_SOAPCS
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("My First SOAP Client!");
+            MyFirstSOAPInterfaceClient client = new
+            MyFirstSOAPInterfaceClient();
+            string text = await
+            client.getHelloWorldAsStringAsync("Karol");
+            Console.WriteLine(text);
+            long text1 = await client.getDaysBetweenDatesAsync("20 12 2022","22 12 2022");
+            Console.WriteLine(text1);
+        }
+    }
+
+}
